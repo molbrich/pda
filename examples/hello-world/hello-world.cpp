361dcf9ec8575e93c02c5cb67882c61baab1d9b3
Markus Olbrich <markus.olbrich@ims.uni-hannover.de>
2021-03-01 21:25:50 +0100

    Bug fixed

--- examples/hello-world/hello-world.cpp
+++ examples/hello-world/hello-world.cpp
@@ -8,7 +8,7 @@
 using namespace Pda; // This imports PDA and PDV
 
 int main () {
-    PDA pda(2,1);  // order: 2, BRVs: 2
+    PDA pda(2,1);  // order: 2, BRVs: 1
     PDV x(pda, 2); // nominal: 2
     x.setDeltaCoeff(0, 0.5); // set deviation of x to 0.5
     std::cout << log(x*x).getMean() << std::endl; // calculate mean of log(x*x)
