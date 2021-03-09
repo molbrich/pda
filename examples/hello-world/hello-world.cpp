9f88e3d004edbc3d2648d47d5fb03886749a3e80
Markus Olbrich <markus.olbrich@ims.uni-hannover.de>
2021-03-09 14:43:50 +0100

    Polishing

--- examples/hello-world/hello-world.cpp
+++ examples/hello-world/hello-world.cpp
@@ -1,6 +1,7 @@
-/* hello-world.cpp
-* Simple demonstration example for the probability distribution arithmetic
-*/
+/* 
+ * hello-world.cpp
+ * Simple demonstration example for the probability distribution arithmetic
+ */
 
 #include <pda_lib.h>
 #include <iostream>
@@ -8,7 +9,7 @@
 using namespace Pda; // This imports PDA and PDV
 
 int main () {
-    PDA pda(2,1);  // order: 2, BRVs: 1
+    PDA pda(2,1);  // order: 2, #BRVs: 1
     PDV x(pda, 2); // nominal: 2
     x.setDeltaCoeff(0, 0.5); // set deviation of x to 0.5
     std::cout << log(x*x).getMean() << std::endl; // calculate mean of log(x*x)
--- examples/rc/rc.cpp
+++ examples/rc/rc.cpp
@@ -1,6 +1,7 @@
-/* rc.cpp
-* Simple demonstration example for the probability distribution arithmetic
-*/
+/* 
+ * rc.cpp
+ * Simple demonstration example for the probability distribution arithmetic
+ */
 
 #include <pda_lib.h>
 #include <iostream>
@@ -9,9 +10,9 @@ using namespace Pda; // This imports PDA and PDV
 
 int main () {
     PDA pda(2,2);  // Order: 2, : Independent variables: 2
-    PDV r(pda, 200); // Mean of r is 200
-    PDV c(pda, 0.01); // Mean of c is 0.01
-    r.setDeltaCoeff(0, 50);   // Set standard deviation of r to 50 (using Delta_0)
+    PDV r(pda, 200);  // Nominal value of r is 200
+    PDV c(pda, 0.01); // Nominal value of c is 0.01
+    r.setDeltaCoeff(0, 50);    // Set standard deviation of r to 50 (using Delta_0)
     c.setDeltaCoeff(1, 0.001); // Set standard deviation of c to 0.01 (using Delta_1)
     PDV t(pda);
     t = r * c;
